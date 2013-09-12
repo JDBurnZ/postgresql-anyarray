59b8386257e53c06ce109b3b8a4c0969b0d1bf10
JDBurnZ <joshuadburns@hotmail.com>
2013-09-12 13:05:29 -0400

    Create anyarray_uniq.sql

---
+++ anyarray_uniq.sql
@@ -0,0 +1,25 @@
+DROP FUNCTION IF EXISTS anyarray_uniq(anyarray);
+CREATE OR REPLACE FUNCTION anyarray_uniq(with_array anyarray)
+	RETURNS anyarray AS
+$BODY$
+	DECLARE
+		-- The variable used to track iteration over "with_array".
+		loop_offset integer;
+
+		-- The array to be returned by this function.
+		return_array with_array%TYPE;
+	BEGIN
+		IF with_array IS NULL THEN
+			return NULL;
+		END IF;
+
+		-- Iterate over each element in "concat_array".
+		FOR loop_offset IN ARRAY_LOWER(with_array, 1)..ARRAY_UPPER(with_array, 1) LOOP
+			IF NOT with_array[loop_offset] = ANY(return_array) THEN
+			return_array = ARRAY_APPEND(return_array, with_array[loop_offset]);
+			END IF;
+		END LOOP;
+
+	RETURN return_array;
+ END;
+$BODY$ LANGUAGE plpgsql;

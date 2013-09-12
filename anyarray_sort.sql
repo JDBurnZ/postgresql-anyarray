899e025811a45e3598c5672cd70501b0909fadd4
JDBurnZ <joshuadburns@hotmail.com>
2013-09-12 13:13:49 -0400

    Create anyarray_sort.sql

---
+++ anyarray_sort.sql
@@ -0,0 +1,17 @@
+DROP FUNCTION IF EXISTS anyarray_sort(anyarray);
+CREATE OR REPLACE FUNCTION anyarray_sort(with_array anyarray)
+	RETURNS SETOF anyarray AS
+$BODY$
+	BEGIN
+		RETURN QUERY SELECT 
+			ARRAY_AGG(sorted_vals.val) AS array_value
+		FROM
+			(
+				SELECT
+					UNNEST(array_vals_to_sort) AS val
+				ORDER BY
+					val
+			) AS sorted_vals
+		;
+	END;
+$BODY$ LANGUAGE plpgsql;

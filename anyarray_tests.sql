9686af273e2be087af6f692865af4193202b2971
JDBurnZ <joshuadburns@hotmail.com>
2013-11-06 15:36:03 -0500

    Create anyarray_tests.sql

---
+++ anyarray_tests.sql
@@ -0,0 +1,36 @@
+/*
+ *
+ * ANYARRAY_REMOVE
+ *
+*/
+
+SELECT
+	-- Expected Result: {1}
+	ANYARRAY_REMOVE(ARRAY[1,2], 2),
+
+	-- Expected Result: {'one'}
+	ANYARRAY_REMOVE(ARRAY['one','two'], 'two'::text),
+
+	-- Expected Result: {1}
+	ANYARRAY_REMOVE(ARRAY[1,2], ARRAY[2]),
+
+	-- Expected Result: {'one'}
+	ANYARRAY_REMOVE(ARRAY['one','two'], ARRAY['two']),
+
+	-- Expected Result: {}
+	ANYARRAY_REMOVE(ARRAY[1], 1),
+
+	-- Expected Result: {}
+	ANYARRAY_REMOVE(ARRAY['one'], 'one'::text),
+
+	-- Expected Result: {}
+	ANYARRAY_REMOVE(ARRAY[1,2], ARRAY[1,2]),
+
+	-- Expected Result: {}
+	ANYARRAY_REMOVE(ARRAY['one','two'], ARRAY['one','two'])
+
+/*
+ *
+ * TODO: Add tests for other functions.
+ *
+*/

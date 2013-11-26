102743e1941b6b9f2b86eb7ec46786f94afc1c00
JDBurnZ <joshuadburns@hotmail.com>
2013-11-26 18:14:16 -0500

    Support for any array data type.

--- anyarray_numeric_only.sql
+++ anyarray_numeric_only.sql
@@ -1,15 +1,15 @@
 DROP FUNCTION IF EXISTS anyarray_numeric_only(anyarray);
 CREATE OR REPLACE FUNCTION anyarray_numeric_only(anyarray)
-	RETURNS anyarray AS
+        RETURNS anyarray AS
 $BODY$
-	SELECT ARRAY(
-		SELECT
-			tbl.val
-		FROM
-			(
-				SELECT UNNEST($1) AS val
-			) AS tbl
-		WHERE
-			tbl.val ~ '^\d+(\.\d+)?$'
-	)
+        SELECT ARRAY(
+                SELECT
+                        array_values.array_value
+                FROM
+                        (
+                                SELECT UNNEST($1) AS array_value
+                        ) AS array_values
+                WHERE
+                        array_values.array_value::TEXT ~ '^\d+(\.\d+)?$'
+        )
 $BODY$ LANGUAGE sql IMMUTABLE;

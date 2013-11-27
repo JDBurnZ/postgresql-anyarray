d12a0bc23cf07e14b4c9ab58a7d69a9392be8e88
JDBurnZ <joshuadburns@hotmail.com>
2013-11-27 16:34:29 -0500

    Initial proposal for ANYARRAY_MAP

--- dev/anyarray_map.sql
+++ dev/anyarray_map.sql
@@ -10,11 +10,16 @@ Every `anyarray` function currently in existance always has the subject
 array (the array which the actions will be performed on,) as the first
 argument.
 
+Examples:
+
+ANYARRAY_REMOVE(ARRAY[1,2,3], 1)
+ANYARRAY_CONCAT(ARRAY[1,2,3], [3,4])
+ANYARRAY_SORT(ARRAY[4,2,9])
+
 Should we continue with this formatting in regards to mapping?
 
-Or, should we change argument ordering to make the logic a little more
-clear?
-
+Or should we make an exception to argument ordering to make the logic a
+little more clear?
 
 */
 

f17e0130f2b96e95d6bd881fc41c874d8febe288
Caleb P. Burns <cpburnz@gmail.com>
2014-01-24 14:12:54 -0500

    Update anyarray_enumerate.sql

--- stable/anyarray_enumerate.sql
+++ stable/anyarray_enumerate.sql
@@ -16,4 +16,6 @@ Unnests the array along with the indices of each element.
 *index* (bigint) is the index of the element within the array starting at 1.
 
 *value* (anyelement) is the element from the array.
+
+NOTE: Multi-dimensional arrays will be flattened as they are with *unnest()*. 
 ';
